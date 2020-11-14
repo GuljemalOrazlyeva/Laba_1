46361592d3cebc946fa1accbb54ce83f9b06119d
GuljemalOrazlyeva <gul2000jemal@yandex.ru>
2020-11-14 13:37:01 +0300

    sozdali class Food

--- src/bsu/rfct/orazlyeva/Laba_1/Food.java
+++ src/bsu/rfct/orazlyeva/Laba_1/Food.java
@@ -1,4 +1,28 @@
 package bsu.rfct.orazlyeva.Laba_1;
 
-public class Food {
+public abstract class Food implements Consumable {
+    private String name = null;
+
+    public Food(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public boolean equals(Object arg0) {
+        if (!(arg0 instanceof Food)) return false;
+        if (this.name==null || ((Food)arg0).name==null) return false;
+        return name.equals(((Food)arg0).name);
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
