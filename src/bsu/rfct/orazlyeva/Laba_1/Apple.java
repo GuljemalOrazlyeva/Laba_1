5b6200bd5b38e8492e8586ad9830f20c25721a83
GuljemalOrazlyeva <gul2000jemal@yandex.ru>
2020-11-14 13:42:40 +0300

    sozdali class Apple

--- src/bsu/rfct/orazlyeva/Laba_1/Apple.java
+++ src/bsu/rfct/orazlyeva/Laba_1/Apple.java
@@ -1,4 +1,28 @@
 package bsu.rfct.orazlyeva.Laba_1;
 
-public class Apple {
+public class Apple extends Food {
+    private String size = null;
+
+    public Apple(String size) {
+        super("Apple");
+        this.size = size;
+    }
+
+    public String getSize() {
+        return this.size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+
+    public String toString() {
+        return super.toString() + " of '" + this.size.toUpperCase() + "' size";
+    }
+
+    public void consume() {
+        System.out.println(this + " is eaten");
+    }
+
 }
